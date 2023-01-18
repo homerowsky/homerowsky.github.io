b0ef928a3e0c280abcb1ffb4f9566cf24f6932cc
HOMER <xerdness@gmail.com>
2023-01-18 19:51:20 +0100

    add "addons" section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const navBar = document.querySelector('.navbar')
 
 	function addBg() {
-		if (window.scrollY >= 1000) {
+		if (window.scrollY >= 300) {
 			navBar.classList.add('scrolled')
 		} else {
 			navBar.classList.remove('scrolled')
